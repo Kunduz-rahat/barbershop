7d64a4526a1e6cf6c450419936be3f7be47da471
Kunduz Rahat <rahatova.kunduz@gmail.com>
2021-07-04 06:45:49 +0300

    inicial

---
+++ script.js
@@ -0,0 +1,181 @@
+ // let num = 5
+// console.log(num)
+
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log( a + b + c )
+
+// let a = 100
+// let b = 2
+// console.log( a * b )
+
+
+// let a = 67
+// let b = 7
+// console.log(  a % b )
+
+// let a = 10
+// let b = 2
+// console.log( a + b )
+// console.log( a - b )
+// console.log( a * b )
+// console.log( a / b )
+
+
+// let c = 15
+// let d = 2
+// let result = c + d
+// console.log(result)
+
+// let a = 30
+// let b = 15
+// let c = a + b
+// let d = 4
+// console.log( c % d )
+
+
+// let a = 17
+// let b = 10
+// let c = a - b
+// let d = 5
+// let result = c + d
+// console.log(result)
+
+
+// let a = "10"
+// let b = "99"
+// let result = a + b
+// console.log(result)
+
+
+// let name = "Аскар"
+// let number = 10
+// let result = name + number
+// console.log( result )
+
+// let a = 20
+// let b = 10
+// a = a + b
+// console.log( a )
+
+
+// let a = 20
+// let b = 10
+// b = a - b
+// console.log(b)
+
+// let a = 90
+// let b = 2
+// a = a + b
+// console.log(a)
+
+// let a = 50
+// let b = 5
+// a = a * b
+// console.log( a )
+
+
+// let a = 660
+// let b = 2
+// a = a / b
+// console.log( a )
+
+
+// let a = 999
+// let b = 9
+// console.log( a % b )
+
+
+// let a = 104
+// let b = 10
+// console.log( a % b )
+
+
+// let a = 23
+// let b = 5
+// console.log( a % b )
+
+//
+// let a = 20
+// let b = 21
+// console.log( a > b )
+
+// let a = 20
+// let b = 21
+// console.log( a < b )
+
+// Интерполяция и конкатенация
+
+
+
+// let name = "Kunduz"
+// console.log( name
+//
+//     )
+
+//
+// let age = 30
+// console.log( age )
+
+
+// let str = " Привет, мир!"
+// console.log( str )
+
+
+// let str1 = "Привет,"
+// let str2 = "мир!"
+// console.log( str1 + " " + str2)
+
+
+// let name = "Kunduz"
+// console.log( "Привет, " + name)
+
+// let age = 30
+// console.log( "Мне " + age + "лет")
+
+// let a = "Земля"
+// let b = 365
+// let c = "7 млдр"
+// let d =  "Солнце"
+// console.log( `В нашем году ${b} дней.  Днем у нас светит ${d}. Население планеты ${a} составляет ${c} человек.`)
+
+
+// let a = "Welcome"
+// let b = "Ivan"
+// console.log( a + "," +" " + b + "!")
+
+// let a = 50
+// console.log( typeof a)
+// //
+// let a = 10
+// let b = 35
+// let result = a + b + ""
+// console.log(typeof result)
+
+// let a = "10"
+// console.log(typeof a)
+
+//
+// let a = "Kunduz"
+// console.log(typeof a)
+
+// let a = 67
+// console.log(typeof a)
+
+// let a = 56
+// let b = 45
+// console.log( a > b )
+// console.log( a < b )
+
+
+ // let str = "информатика"
+ // console.log(str.length)
+
+
+ // let str = "Мы выполняем домашнее задание"
+ // console.log(str.split(" ")[2].length)
+ //
+ //
+ // split применяется только к строкам, разбивая строку на массив
+
